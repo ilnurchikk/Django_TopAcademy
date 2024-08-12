41eb084bf8a2a598f4943da05840ed808ae9acdb
Marsel <99655240+FeklushAAA@users.noreply.github.com>
2024-08-12 20:39:17 +0300

    продол
    обувь

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    #Данное наследование показывает нам, что данное поле в БД будет иметь тип данных чарфилд
+    name = models.CharField(max_length=256, unique=True)
+    #Отличие чарфилда от текстфилда в том, что етксфилд принимамет в себя большое количество текста
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9999, decimal_places=9)
+    #Количество товаров на складе
+    quontity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
